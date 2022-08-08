dc5cde85abf39dd3ccb63bb5841637c1ae6fbcec
Julian Sarza <julian2570@gmail.com>
2022-08-07 20:39:04 -0700

    added gitignore and files

---
+++ src/Queues.java
@@ -0,0 +1,9 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Queues {
+	public static void main(String[] args) {
+		
+		Queue<Integer> myQueue = new LinkedList<Integer>();
+	}
+}
